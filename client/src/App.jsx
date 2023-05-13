680552efedfadc71fcba4236a947b980f77fab97
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-05-14 02:24:10 +0530

    base

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,27 +15,50 @@ function App() {
     <div className="App">
       <div className="navbar">
         <div className="logo">
-        <canvas id="canvas3d"></canvas>
-        <img src="logo.png" alt="" />
+          <canvas id="canvas3d"></canvas>
+          <img src="logo.png" alt="" />
         </div>
         <div className="options">
-          <p>HOME</p>
-          <p>ORGANIZATION</p>
-          <p>CALENDAR</p>
-          <p>MEMBER</p>
-          <p>SERVICE</p>
-          <p>BLOG</p>
-          <p>CONTACT</p>
-          <p>DOWNLOAD</p>
+          <p>
+            HOME <p></p>
+          </p>
+          <p>
+            ORGANIZATION <p></p>
+          </p>
+          <p>
+            CALENDAR <p></p>
+          </p>
+          <p>
+            MEMBER <p></p>
+          </p>
+          <p>
+            SERVICE <p></p>
+          </p>
+          <p>
+            BLOG <p></p>
+          </p>
+          <p>
+            CONTACT <p></p>
+          </p>
+          <p>
+            DOWNLOAD <p></p>
+          </p>
         </div>
       </div>
       <div className="webBody">
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-          facere, repellendus, perferendis sed qui aspernatur rem nostrum
-          officiis provident culpa enim, minus porro deserunt! Eligendi
-          reiciendis consequatur iure excepturi.
-        </h1>
+        <div className="hero">
+          <h1>
+            <h1>Efficiency Is Claver Laziness</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
+            facere, repellendus, perferendis sed qui aspernatur rem nostrum
+            officiis provident culpa enim, minus porro deserunt! Eligendi
+            reiciendis consequatur iure excepturi.
+          </h1>
+          <h1></h1>
+        </div>
+        <div className="content">
+          
+        </div>
       </div>
     </div>
   );
