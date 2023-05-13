531a5b8de8626472fad8802b745a71ef58be83a8
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-05-14 03:09:07 +0530

    how to change text selection color css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import "./App.scss";
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
 
 function App() {
   // useEffect(() => {
@@ -10,13 +10,12 @@ function App() {
 
   //   app.load("https://prod.spline.design/Rj7Gnb9FoqvWt6Nq/scene.splinecode");
   // }, []);
-  
-  
+
   return (
     <div className="App">
       <div className="navbar">
         <div className="logo">
-        <Spline scene="https://prod.spline.design/Rj7Gnb9FoqvWt6Nq/scene.splinecode" />
+          <Spline scene="https://prod.spline.design/Rj7Gnb9FoqvWt6Nq/scene.splinecode" />
           <img src="logo.png" alt="" />
         </div>
         <div className="options">
@@ -50,16 +49,19 @@ function App() {
         <div className="hero">
           <h1>
             <h1>Efficiency Is Claver Laziness</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod
-            facere, repellendus, perferendis sed qui aspernatur rem nostrum
-            officiis provident culpa enim, minus porro deserunt! Eligendi
-            reiciendis consequatur iure excepturi.
+            Today, karate is a martial art known and respected worldwide as an
+            effective combative art. Its birthplace, the small islands of
+            Okinawa in Japan, was once known as Ryukyu, an independent kingdom.
+            Shorin Ryu is one of Okinawa’s three main streams of Karate (Goju
+            Ryu, Uechi Ryu, Shorin Ryu). In 1933, Master Choshin Chibana named
+            Shorin Ryu. In 1948, they organized Okinawa Shorin Ryu Karate-do
+            Association. They took over the Shuri-te stream. There is a Dojo not
+            only in Okinawa but also overseas. Shorin Ryu Shubukan was founded
+            by Sensei Joki Uema.
           </h1>
           <h1></h1>
         </div>
-        <div className="content">
-
-        </div>
+        <div className="content"></div>
       </div>
     </div>
   );
