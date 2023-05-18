aef5914544f1fccf8ad8111e87c37ded7ddf1094
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-05-18 23:23:33 +0530

    karate images

--- client/src/Component/Home/Home.jsx
+++ client/src/Component/Home/Home.jsx
@@ -13,6 +13,7 @@ export default function Home() {
           there is a diviner instinct to love <i>&nbsp;</i>
         </p>
       </section>
+
       <section className="intro">
         <div className="define">
           <h1>The Way</h1>
@@ -40,50 +41,21 @@ export default function Home() {
           <div></div>
         </div>
       </section>
+
       <section className="gallery">
         <h1 className="galleryTop">Gallery</h1>
         <div>
           <div className="image">
-            <img
-              src="https://images.unsplash.com/photo-1604871000636-074fa5117945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-              alt="image"
-            />
+            <img src="chibana.jpg" alt="image" />
           </div>
           <div className="image">
-            <img
-              src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-              alt="image"
-            />
+            <img src="mag.jpg" alt="image" />
           </div>
           <div className="image">
-            <img
-              src="https://images.unsplash.com/photo-1505178041309-ad46d2e4207b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="image"
-            />
+            <img src="uema.jpg" alt="image" />
           </div>
           <div className="image">
-            <img
-              src="https://images.unsplash.com/photo-1497114046243-1154db4f4abf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=948&q=80"
-              alt="image"
-            />
-          </div>
-          <div className="image">
-            <img
-              src="https://images.unsplash.com/photo-1573655349936-de6bed86f839?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGFic3RyYWN0JTIwJTIweWVsbG93fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt="image"
-            />
-          </div>
-          <div className="image">
-            <img
-              src="https://images.unsplash.com/photo-1541356665065-22676f35dd40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
-              alt="image"
-            />
-          </div>
-          <div className="image">
-            <img
-              src="https://images.unsplash.com/photo-1509114397022-ed747cca3f65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YWJzdHJhY3QlMjAlMjBza3l8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-              alt="image"
-            />
+            <img src="yasuhiro.jpg" alt="image" />
           </div>
           <div className="image">
             <img
@@ -92,13 +64,23 @@ export default function Home() {
             />
           </div>
           <div className="image">
+            <img src="mag2.jpg" alt="image" />
+          </div>
+          <div className="image">
+            <img src="takeshi.jpg" alt="image" />
+          </div>
+          <div className="image">
             <img
-              src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGFic3RyYWN0JTIwJTIwc2t5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              src="https://images.unsplash.com/photo-1509114397022-ed747cca3f65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YWJzdHJhY3QlMjAlMjBza3l8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
               alt="image"
             />
           </div>
+          <div className="image">
+            <img src="sensei.jpg" alt="image" />
+          </div>
         </div>
       </section>
+
       <section className="blog">
         <div className="blogLine"></div>
         <h1>Never Fade Away</h1>
