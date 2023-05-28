9a7daa9d2faa4f1c4a7c3c01be1de2e43bbbc6c1
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-05-28 21:29:52 +0530

    mobile burger

--- client/src/Component/Navbar/Navbar.jsx
+++ client/src/Component/Navbar/Navbar.jsx
@@ -6,15 +6,17 @@ import "./Navbar.scss";
 
 export default function Navbar(props) {
   const [isBurger, setIsBurger] = useState(true);
-  // const [burger, setBurger] = useState('');
+  const [isOption, setIsOption] = useState(false);
   const [burgerStyle, setBurgerStyle] = useState({
     height: "1rem",
-    width: "4rem",
     transition: "all .3s ease-in-out",
   });
   const [empty, setEmpty] = useState({ height: "0rem", display: "none" });
   const [xRotate, setXRotate] = useState({});
   const [yRotate, setYRotate] = useState({});
+  const [zGradient, setZGradient] = useState({
+    background: "linear-gradient(to left, #A64B37 20%, transparent 100%)",
+  });
 
   const [position, setPosition] = useState({ top: "0rem" });
   const [lastScrollTop, setLastScrollTop] = useState(Infinity);
@@ -78,64 +80,96 @@ export default function Navbar(props) {
   );
 
   function setStyle() {
+    setIsOption(true);
     setBurgerStyle({
-      height: "50vh",
+      height: "20rem",
       width: "8rem",
       transition: "all .3s ease-in-out",
+      background: "linear-gradient(to top, #D2D9D3, transparent)",
     });
     setEmpty({ height: "3rem" });
-    setXRotate({ transform: "rotate(15deg)" });
-    setYRotate({ transform: "rotate(-15deg)" });
+    setXRotate({
+      transform: "rotate(15deg)",
+      background: "radial-gradient(circle, #d1a59b 20%, transparent 100%)",
+    });
+    setYRotate({
+      transform: "rotate(-15deg)",
+      background: "radial-gradient(circle, #d1a59b 20%, transparent 100%)",
+    });
   }
   function removeStyle() {
+    setIsOption(false);
     setBurgerStyle({
       height: "1rem",
-      width: "4rem",
       transition: "all .3s ease-in-out",
     });
     setEmpty({ height: "0rem", display: "none" });
     setXRotate({});
     setYRotate({});
-    console.log("lol");
+  }
+  function lineStyle() {
+    setZGradient({
+      background: "radial-gradient(circle, #A64B37 20%, transparent 100%)",
+    });
+    if (!isOption) {
+      setXRotate({background: "radial-gradient(circle, #d1a59b 20%, transparent 100%)"});
+      setYRotate({background: "radial-gradient(circle, #d1a59b 20%, transparent 100%)"});
+    }
+  }
+  function notLineStyle() {
+    if (!isOption) {
+      setZGradient({
+        background: "linear-gradient(to left, #A64B37 20%, transparent 100%)",
+      });
+      setXRotate({background: "linear-gradient(to left, #d1a59b 20%, transparent 100%)"});
+      setYRotate({background: "linear-gradient(to left, #d1a59b 20%, transparent 100%)"});
+    }
   }
 
   const burger = (
     <>
-      <nav className="burger" style={burgerStyle}>
+      <nav
+        className="burger"
+        style={burgerStyle}
+        onMouseOver={lineStyle}
+        onMouseOut={notLineStyle}
+      >
         <div className="empty" style={empty} onClick={removeStyle}></div>
         <div className="burgerLine" onClick={setStyle}>
           <div className="x" style={xRotate}></div>
           <div className="y" style={yRotate}></div>
-          <nav className="optionsMobo">
-            <NavLink to="/" className="menuDivMobo">
-              <p>HOME</p>
-            </NavLink>
+          {isOption && (
+            <nav className="optionsMobo">
+              <NavLink to="/" className="menuDivMobo">
+                <p>HOME</p>
+              </NavLink>
 
-            <NavLink to="/" className="menuDivMobo">
-              <p>ORGANIZATION</p>
-            </NavLink>
+              <NavLink to="/" className="menuDivMobo">
+                <p>ORGANIZATION</p>
+              </NavLink>
 
-            <NavLink to="/kata" className="menuDivMobo">
-              <p>KATA</p>
-            </NavLink>
+              <NavLink to="/kata" className="menuDivMobo">
+                <p>KATA</p>
+              </NavLink>
 
-            <NavLink to="/" className="menuDivMobo">
-              <p>MEMBER</p>
-            </NavLink>
+              <NavLink to="/" className="menuDivMobo">
+                <p>MEMBER</p>
+              </NavLink>
 
-            <NavLink to="/" className="menuDivMobo">
-              <p>SERVICE</p>
-            </NavLink>
+              <NavLink to="/" className="menuDivMobo">
+                <p>SERVICE</p>
+              </NavLink>
 
-            <NavLink to="/blog" className="menuDivMobo">
-              <p>BLOG</p>
-            </NavLink>
+              <NavLink to="/blog" className="menuDivMobo">
+                <p>BLOG</p>
+              </NavLink>
 
-            <NavLink to="/" className="menuDivMobo">
-              <p>CONTACT</p>
-            </NavLink>
-          </nav>
-          <div className="z"></div>
+              <NavLink to="/" className="menuDivMobo">
+                <p>CONTACT</p>
+              </NavLink>
+            </nav>
+          )}
+          <div className="z" style={zGradient}></div>
         </div>
       </nav>
     </>
